d677cc1fb7d57a284c1e7fae6ca8117c6cbf96e3
Jota Uribe <jjotaum@gmail.com>
2023-11-11 10:17:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SwiftyTree
+A MacOS command like tool to generate Tree representation from a given directory path
---
+++ SwiftyTree/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftyTree
+//
+//  Created by Jota Uribe on 21/10/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
